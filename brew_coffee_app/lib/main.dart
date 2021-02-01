1bb667a57f9355bf769f8242d30a8aa88c772692
Opssy <opeyemib39@gmail.com>
2021-02-01 13:58:03 +0100

    coffee app ongoing

---
+++ brew_coffee_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:brew_coffee_app/screens/wrapper.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Wrapper(),
+    );
+  }
+}
